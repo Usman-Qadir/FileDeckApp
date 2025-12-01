982f221abb69a06d1442ff7a078db1ae4256dfa6
Usman qadir <usman.qadir.cps@gmail.com>
2025-12-01 11:50:00 +0500

    signup ui

--- src/app/demo/pages/authentication/register/register.component.ts
+++ src/app/demo/pages/authentication/register/register.component.ts
@@ -1,49 +1,48 @@
-// angular import
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../../../../services/auth.service';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule, ReactiveFormsModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
   model = {
     firstName: '',
     lastName: '',
     email: '',
     password: ''
+  };
+
+  isLoading = false;
+
+  constructor(private auth: AuthService) {}
+
+  onSubmit() {
+    this.isLoading = true;
+
+    const userObj = {
+      email: this.model.email,
+      password: this.model.password,
+      displayName: `${this.model.firstName} ${this.model.lastName}`
+    };
+
+    this.auth.registerUser(userObj).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        alert('Registration Successful');
+        console.log('Registration Successful', res);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        alert(err.error?.message || 'Registration Failed');
+        console.error('Registration Failed', err);
+      }
+    });
   }
-   
-  isLoading=false;
-  constructor(private auth:AuthService){}
-
-  onSubmit(){
-    this.isLoading=true;  
-  
-const userObj= {
-  email: `${this.model.email}`,
-  password: `${this.model.password}`,
-  displayName: `${this.model.firstName} ${this.model.lastName}`
-}
-
-this.auth.registerUser(userObj).subscribe({
-  next:(res)=>{
-    this.isLoading=false;
-    alert('Registration Successful');
-    console.log('Registration Successful', res);
-  },
-  error:(err)=>{
-    this.isLoading=false;
-    alert(err.error?.message || 'Registration Failed');
-    console.error('Registration Failed', err);
-  }
-})
-
-}
-
 }
